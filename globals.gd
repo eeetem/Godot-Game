eb685c69aedff0dc2422eaee80c84d416e3bd6ed
eeetem <denik302@gmail.com>
2022-05-29 20:42:47 +0100

    dead game xd

--- globals.gd
+++ globals.gd
@@ -4,11 +4,21 @@ extends Node
 var showNavDebug = false;
 var DoNavDemo = false;
 
+var sfxPlayer = preload("res://SoundEmitter.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func PlaySound(sfx,location,distance):
+	if distance < 0:
+		distance = 10;
+	var scale = 1.0/(520.0/distance)
+	var instance = sfxPlayer.instance();
+	instance.distance = distance;
+	instance.sfx = sfx;
+	instance.targetSize = scale;
+	instance.position = location
+	get_tree().get_root().get_node("Control/ViewportContainer/Viewport/World").add_child(instance);
+
